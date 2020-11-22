33e75e1bfa6724642d1993d316c314881448a0a3
zzp <702638473@qq.com>
2020-11-22 15:02:24 +0800

    now can send message to your friend

--- simple-project/src/main/java/com/six/controller/loginController.java
+++ simple-project/src/main/java/com/six/controller/loginController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +22,13 @@ public class loginController {
 	@PostMapping
 	public String login(@RequestParam("userId") String userId,
 						@RequestParam("password") String password,
-						Map<String, Object> map, HttpSession session) {
+						Map<String, Object> map, HttpSession session, Model model) {
 		User loginUser = userMapper.getOne(userId);
 		if (userId.equals("admin") && password.equals("123456")) {
 			session.setAttribute("loginUser", "admin");
 			return "redirect:home";
 		} else if (loginUser != null && loginUser.getPassword().equals(password)){
-			session.setAttribute("loginUser", loginUser.getName());
+			session.setAttribute("loginUser", loginUser.getId());
 			return "redirect:home";
 		}
 		else {
